1d7f6c071cdb08ac451dda6422e96755598e7274
JeetsibeshSahoo <jeetsibes05@gmail.com>
2026-07-10 15:35:47 +0530

    try out

--- Week1/Day1/frontend/src/App.jsx
+++ Week1/Day1/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import Register from "./pages/Register"
+import Login from "./pages/Login"
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
--- Week1/Day1/frontend/src/pages/Register.jsx
+++ Week1/Day1/frontend/src/pages/Register.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from 'react-router-dom';
+import { register } from '../features/auth/authSlice';
 
-function Register() {
+const Register = () => {
+
+    const dispatch = useDispatch();
+    const { isLoading, error } = useSelector((state) => state.auth);
+
+    const [formData, setFormData] = useState({
+        name : "",
+        email : "",
+        password : "",
+        age : ""
+    });
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name] : e.target.value
+        });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const result = await dispatch(register(formData));
+        if(result.meta.requestStatus === "fulfilled") {
+            alert ("Registered Successfully");
+            navigate("/login");
+        }
+    };
+
   return (
     <div>
-      
+        <h2>Register</h2>
+
+        <form onSubmit={handleSubmit}>
+            <input 
+            name='name' 
+            placeholder='Your name...' 
+            type="text"
+            onChange={handleChange} 
+            />
+            <input 
+            name='email' 
+            placeholder='Your Email...' 
+            type="email"
+            onChange={handleChange} 
+            />
+            <input 
+            name='password' 
+            placeholder='Your password...' 
+            type="password"
+            onChange={handleChange} 
+            />
+            <input 
+            name='age' 
+            placeholder='Your age...' 
+            type="number"
+            onChange={handleChange} 
+            />
+            <button type='submit'>
+                {isLoading ? "Loading..." : "Register"}
+            </button>
+        </form>
+        {error && <p style={{color : "red"}}>{error}</p>}
     </div>
   )
 }
 
-export default Register;
+export default Register
