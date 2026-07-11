7718aa870bd76eb2912ca703bda1a6a607266718
JeetsibeshSahoo <jeetsibes05@gmail.com>
2026-07-11 15:49:11 +0530

    add changes in register ui

--- Week1/Day1/frontend/src/pages/Register.jsx
+++ Week1/Day1/frontend/src/pages/Register.jsx
@@ -34,39 +34,56 @@ const Register = () => {
     };
 
   return (
-    <div>
-        <h2>Register</h2>
+    <div className='min-h-screen flex items-center justify-center bg-gray-100'>
+        <div className='bg-white p-8 rounded-2xl justify-center shadow-md w-full max-w-md'>
+            <h2 className='text-2xl font-bold mb-6 text-center'>
+                Create Account
+            </h2>
 
-        <form onSubmit={handleSubmit}>
-            <input 
-            name='name' 
-            placeholder='Your name...' 
-            type="text"
-            onChange={handleChange} 
-            />
-            <input 
-            name='email' 
-            placeholder='Your Email...' 
-            type="email"
-            onChange={handleChange} 
-            />
-            <input 
-            name='password' 
-            placeholder='Your password...' 
-            type="password"
-            onChange={handleChange} 
-            />
-            <input 
-            name='age' 
-            placeholder='Your age...' 
-            type="number"
-            onChange={handleChange} 
-            />
-            <button type='submit'>
-                {isLoading ? "Loading..." : "Register"}
-            </button>
-        </form>
-        {error && <p style={{color : "red"}}>{error}</p>}
+            <form>
+                <input 
+                name='name'
+                placeholder='Enter your name...'
+                type="text" 
+                onChange={handleChange}
+                className='w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400'
+                />
+                <input 
+                name='email'
+                placeholder='Enter your email...'
+                type="email" 
+                onChange={handleChange}
+                className='w-full p-3 border rounded-lg'
+                />
+                <input 
+                name='password'
+                placeholder='Enter your password...'
+                type="password" 
+                onChange={handleChange}
+                className='w-full p-3 border rounded-lg'
+                />
+                <input 
+                name='age'
+                placeholder='Enter your age...'
+                type="number" 
+                onChange={handleChange}
+                className='w-full p-3 border rounded-lg'
+                />
+
+                <div className='mt-5'>
+                    <button type='submit' className='w-full bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition'>
+                        {isLoading ? "Creating..." : "Register"}
+                    </button>
+                    {
+                        error && (
+                            <p className='text-red-500 text-sm mt-4 text-center'>
+                                {error}
+                            </p>
+                        )
+                    }
+                </div>
+            </form>
+        </div>
     </div>
   )
 }
